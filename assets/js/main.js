99b397a0b283e544c044df5f14b211d2a4f511e1
Hazvi Nurafgan <hazvinurafgan@gmail.com>
2022-01-08 00:48:24 +0700

    Reuploaded

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const signUp = () => {
+    window.location.href = 'register.html';
+}
+const login = () => {
+    window.location.href = 'index.html';
+}
+
+const newLogin = () => {
+    window.location.href = './menu/home.html'
+}
+
+const backLogin = () => {
+    window.location.href = "../index.html";
+}
+const backSignUp = () => {
+    window.location.href = "../register.html";
+}
+
+const guest = () => {
+    window.location.href = './menu/guest.html';
+}
+
+const home = () => {
+    window.location.href = './menu/home.html';
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Task 5 Vehicle Rental Front-End
+
+## Using Begginner Code :
+
+<ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>JS (onclick)</li>
+<li>Bootstrap Framework</li>
+</ul>
+
+<br/>
+
+## View At Netlify
+
+Link here : <a href ="https://vehicle-rental-font-end.netlify.app/">Netlify Task 5</a>
+
+#### form other file (not link href & button)
+
+Link : "https://vehicle-rental-font-end.netlify.app/menu/view-more.html"
+
+or click <a href = "https://vehicle-rental-font-end.netlify.app/menu/view-more.html">Here!</a>
